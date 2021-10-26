4b13a7ded95429bfe2c2d4f3f808507aa1147bf4
Dharambir Kumar <kumar.dharambir99@gmail.com>
2021-10-26 15:23:50 +0530

    Initial commit

---
+++ mongodb.mjs
@@ -0,0 +1,40 @@
+// const mongodb = require('mongodb');
+// const MongoClient = mongodb.MongoClient;
+// const ObjectId = mongodb.ObjectId;
+
+// const { MongoClient, ObjectId } = require('mongodb');
+import { MongoClient, ObjectId } from "mongodb";
+
+const id = new ObjectId;
+
+// console.log(id);
+const connectionURL = 'mongodb://127.0.0.1:27017';
+const databaseName = 'task-manager';
+
+MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
+    if (error) {
+        return console.log('Unable to connect to DB!');
+    }
+    const db = client.db(databaseName);
+    // db.collection('users').findOne({ role: 'Developer' }, (error, user) => {
+    //     if (error) {
+    //         return console.log('Unable to find');
+    //     }
+    //     console.log(user)
+    // });
+
+    db.collection('users').find({ role: 'Developer' }).toArray((error, users) => {
+        console.log(users);
+    });
+
+
+    // db.collection('users').insertOne({
+    //     name: 'Dharambir',
+    //     role: 'Developer'
+    // }, (error, result) => {
+    //     if (error) {
+    //         return console.log("Unable to insert")
+    //     }
+    //     console.log(result)
+    // })
+})
